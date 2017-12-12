3f7e889a28d1e3f1ce6d09537f9efd835f05cedb
T-Berger <32510636+T-Berger@users.noreply.github.com>
2017-12-12 11:01:18 +0100

    Added Header/Toolbar from mainpage

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-tabs id="header" dark>
+    <v-tabs-bar class="black" dark>
+      <v-tabs-slider class="grey darken-3w"></v-tabs-slider>
+        <v-tabs-item class="v-tabs-item" id="firstTab">
+          <v-icon large dark>mdi-arrow-left-bold</v-icon>
+        </v-tabs-item>
+      <v-spacer></v-spacer>
+        <v-tabs-item centered class="v-tabs-item">
+           <span class="dimgrey--text">Hallo ich bin</span>
+          <v-icon large dark>mdi-arrow-right-bold</v-icon>
+        </v-tabs-item>
+      <v-spacer></v-spacer>
+        <v-tabs-item class="v-tabs-item">
+          <v-icon large dark>mdi-arrow-right-bold</v-icon>
+        </v-tabs-item>
+    </v-tabs-bar>
+  </v-tabs>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .tabs__bar{
+    height: var(--laengeHeader-);
+  }
+  #header{
+    position: fixed;
+    top: 0;
+  }
+  .theme--dark{
+    color:  dimgrey !important;
+
+  }
+  .v-tabs-item:hover {
+    background: #ff6e42;
+    color: #fffcfb;
+  }
+  .v-tabs-item:hover:before, .v-tabs-item:hover:after {
+    -webkit-transition: all .25s linear;
+    transition: all .25s linear;
+  }
+  .v-tabs-item:hover:before {
+    background: #b65234;
+  }
+  .v-tabs-item:hover:after {
+    background: #b65234;
+  }
+  .v-tabs-item:before, .v-tabs-item:after {
+    content: '';
+    position: absolute;
+  }
+  .v-tabs-item:after {
+    background: #181818;
+  }
+  .v-tabs-item:before {
+    background: #121212;
+  }
+  #firstTab{
+    margin-left: 30px;
+  }
+</style>
