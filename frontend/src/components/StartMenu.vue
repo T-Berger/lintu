b6208de193455a993460603e3350ea3b5e10af9e
T-Berger <32510636+T-Berger@users.noreply.github.com>
2017-12-16 18:23:40 +0100

    Task-Card expand on icon click

--- frontend/src/components/StartMenu.vue
+++ frontend/src/components/StartMenu.vue
@@ -6,19 +6,19 @@
          <v-layout row wrap>
            <v-flex
              xs3
-             v-for="n in 9"
-                  :key="n"
+             v-for="(card, index) in cards"
+                  :key="card.id"
                 >
                   <v-card raised>
-                    <v-btn centered v-on:click="showlabel = !showlabel">
+                    <v-btn centered v-on:click="toggle(card)">
                     <v-icon x-large>
                       fa-cogs
                     </v-icon>
                     </v-btn>
-                      <v-layout v-show="showlabel==true">
+                      <v-layout v-show="card.visible" v-model="form.parent_id[index]">
                         <v-card-title primary-title>
                           <div>
-                            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+                            <h3 class="headline mb-0">{{ card.name + " " +  'name_id' + index  + card.id  }}</h3>
                             <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
                           </div>
                         </v-card-title>
@@ -40,7 +40,23 @@
       name: 'start-menu',
       data () {
         return {
-          showlabel: false
+          cards: [
+            {name: 'MacBook Air', id: 1, visible: false},
+            {name: 'MacBook Pro', id: 2, visible: false},
+            {name: 'Lenovo W530', id: 3, visible: false},
+            {name: 'Acer Aspire One', id: 4, visible: false}
+          ],
+          form: {
+            parent_id: []
+          },
+          showlabel: false,
+          show0: false,
+          show1: false
+        }
+      },
+      methods: {
+        toggle (card) {
+          card.visible = !card.visible
         }
       }
     }
