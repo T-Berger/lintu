38f01cc42f13462ed622a591824505a70f4b2cda
T-Berger <32510636+T-Berger@users.noreply.github.com>
2017-12-20 22:03:54 +0100

    undeletable label added

--- frontend/src/components/contentpage/task.vue
+++ frontend/src/components/contentpage/task.vue
@@ -1,7 +1,6 @@
 <template>
 
   <div id="editor">
-    
     <div v-html="compiledMarkdown">
     </div>
   </div>
--- frontend/src/components/contentpage/terminal.vue
+++ frontend/src/components/contentpage/terminal.vue
@@ -6,7 +6,9 @@
       <span>{{ historyinput[index]}}</span>
     </div>
     <label for = terminalInput>hallo ----></label>
-    <input id = "terminalInput" v-model="input" type="text" v-on:keyup.enter="saveString()">
+    <input id = "terminalInput" v-model="input" type="text" v-on:keyup.enter="saveString()"
+   v-on:keydown.delete.prevent="notDeletableLabel()">
+    <!--v-on:keyup.delete="notDeletableLabel()"-->
   </div>
 </template>
 
@@ -16,15 +18,44 @@
       data () {
         return {
           historyinput: [],
-          input: ''
+          input: 'USERNAME ',
+          inputLabel: 'USERNAME '
         }
       },
       methods: {
         saveString () {
           console.log(this.input)
           this.historyinput.push(this.input)
-          this.input = ''
+          this.input = this.inputLabel
+          console.log(this.inputLabel)
+        },
+        notDeletableLabel () {
+          var inputDeletable = this.input.substr(this.input.length - (this.input.length - this.inputLabel.length))
+          console.log(inputDeletable)
+          console.log(this.inputLabel)
+          console.log(this.inputLabel.concat(inputDeletable))
+          this.input = this.inputLabel.concat(inputDeletable)
+          console.log(this.input)
+          this.input = this.input.slice(0, (this.input.length - 1))
+          console.log(this.input)
+
+          // if (this.inputLabel.length === this.input.length){
+          //
+          // }
+          /* WIRD ANDERS GELÖST MIT
+          var labelTemplate = this.inputLabel.substr(this.inputLabel.length - 4)
+          console.log(labelTemplate)
+          console.log(labelTemplate.charAt(labelTemplate.length - 1))
+          console.log(this.input.charAt(this.input.length - 1))
+          if (labelTemplate.charAt(labelTemplate.length - 1) === this.input.charAt(this.input.length - 1)) {
+            console.log('not delete')
+          } else {
+            this.input.slice(0, ((this.input.length - 1) - 1)) // DAS HIER IST UNNÖTIG DA SOWIESO DAS BACKSPACE AUSGEFÜHRT WIRD
+          } */
         }
+      // },
+      // computed: {
+      //   // inputLabelPlusInput: {
       }
     }
 </script>
