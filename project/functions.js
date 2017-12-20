bff63055666f91d200097efd2ced2af347ad6df8
Kyo <Zilver361@gmx.de>
2017-12-20 10:01:33 +0100

    konsolen versuch (funktioniert noch nicht) verbesserung erfolgt im neuen branch

--- project/functions.js
+++ project/functions.js
@@ -1,8 +1,43 @@
 //Biblothek Autosize js
-autosize(document.getElementById("input"));
+/*autosize(document.getElementById("input"));
 
 //onkeyup="auto_grow(this)"
 function auto_grow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight)+"px";
-}
+}*/
+/*function sendAnswer() {
+    var newInput = document.createElement("input");
+    newInput.appendChild();
+}*/
+/*$('#terminalInput').bind('keyPress' ,function (e) {
+    if (e.keyCode == 13){
+        var newInput = document.createElement("input");
+        newInput.appendChild();
+    }
+});*/
+/*$("#terminalInput").keypress(function(e) {
+    if(e.which == 13) {
+        document.getElementById("terminal").appendChild(document.createElement("p"));
+        document.getElementById("terminal").appendChild(document.createElement("input", id="terminalInput"));
+    }
+});*/
+
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            historyInput: [],
+            input: ""
+        }
+        },
+    methods:{
+        saveString(){
+            console.log(this.input)
+            this.historyInput.push(this.input)
+            this.input= ""
+        }
+    }
+
+})
