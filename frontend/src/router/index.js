9cc0053857dee360b644f5b82e41fef6172d36f2
Paddy4 <32911136+Paddy4@users.noreply.github.com>
2018-01-17 13:20:08 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,7 +5,62 @@ import Login from '@/components/Login'
 import ContentPage from '@/components/ContentPage'
 Vue.use(Router)
 
-export default new Router({
+export default new Rou/* eslint-disable */
+import Vue from 'vue'
+import Router from 'vue-router'
+import StartMenu from '@/components/StartMenu'
+import Login from '@/components/Login'
+import ContentPage from '@/components/ContentPage'
+import SignUp from '@/components/SignUp'
+import firebase from 'firebase'
+Vue.use(Router)
+
+let router = new Router({
+  routes: [
+    {
+      path: '*',
+      redirect: '/login'
+    },
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/',
+      name: 'ContentPage',
+      component: ContentPage
+    },
+    {
+      path: '/sign-up',
+      name: 'signUp',
+      component: SignUp
+    },
+    {
+      path: '/StartMenu',
+      name: 'StartMenu',
+      component: StartMenu,
+      meta: {
+        requiresAuth: true,
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser;
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('startmenu')
+  else next()
+})
+export default router
+ter({
   routes: [
     {
       path: '/register',
