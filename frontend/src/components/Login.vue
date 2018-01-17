9e03a2e2fff99780e653f56cb9b6bf283287cd8f
Paddy4 <32911136+Paddy4@users.noreply.github.com>
2018-01-17 13:16:22 +0100

    new Login

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,42 +1,60 @@
 <template>
-  <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <panel title="Login">
-        <v-text-field
-          label="Email"
-          v-model="email"
-        ></v-text-field>
-        <br>
-        <v-text-field
-          label="Password"
-          type="password"
-          v-model="password"
-        ></v-text-field>
-        <br>
-        <div class="danger-alert" v-html="error" />
-        <br>
-        <v-btn
-          dark
-          class="cyan"
-          @click="login">
-          Login
-        </v-btn>
-      </panel>
-    </v-flex>
-  </v-layout>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button v-on:click="signIn">Connection</button>
+    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
+  </div>
 </template>
 
 <script>
+  import firebase from 'firebase'
+
   export default {
-    data () {
+    name: 'login',
+    data: function () {
       return {
         email: '',
-        password: '',
-        error: null
+        password: ''
+      }
+    },
+    methods: {
+      signIn: function () {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          (user) => {
+            this.$router.replace('StartMenu')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        )
       }
     }
   }
 </script>
 
-<style scoped>
+<style scoped>  /* "scoped" attribute limit the CSS to this component only */
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
 </style>
+
