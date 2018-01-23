9481f6a45adce8ba952afdf3f2ffcd080ba4dc13
Paddy <patrick.karlbauer@web.de>
2018-01-23 17:56:08 +0100

    Update Layout

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,7 +3,27 @@
     <h3>Sign In</h3>
     <input type="text" v-model="email" placeholder="Email"><br>
     <input type="password" v-model="password" placeholder="Password"><br>
-    <button v-on:click="signIn">Connection</button>
+
+    <div class="text-xs-center">
+      <v-bottom-sheet v-model="sheet">
+        <v-btn slot="activator" color="green" dark v-on:click="signIn">Connect</v-btn>
+        <v-list>
+          <v-subheader>Open in</v-subheader>
+          <v-list-tile
+            v-for="tile in tiles"
+            :key="tile.title"
+            @click="sheet = false"
+          >
+            <v-list-tile-avatar>
+              <v-avatar size="32px" tile>
+                <img :src="`/static/doc-images/bottom-sheets/${tile.img}`" :alt="tile.title">
+              </v-avatar>
+            </v-list-tile-avatar>
+            <v-list-tile-title>{{ tile.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-bottom-sheet>
+    </div>
     <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
   </div>
 </template>
--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="sign-up">
-    <p>Let's create a new account !</p>
+    <h3>Let's create a new account !</h3><br>
     <input type="text" v-model="email" placeholder="Email"><br>
     <input type="password" v-model="password" placeholder="Password"><br>
-    <button v-on:click="signUp">Sign Up</button>
+    <div class="text-xs-center">
+      <v-bottom-sheet v-model="sheet">
+        <v-btn slot="activator" color="green" dark v-on:click="signUp">Sign Up</v-btn>
+        <v-list>
+          <v-list-tile
+            v-for="tile in tiles"
+            :key="tile.title"
+            @click="sheet = false"
+          >
+            <v-list-tile-avatar>
+              <v-avatar size="32px" tile>
+                <img :src="`/static/doc-images/bottom-sheets/${tile.img}`" :alt="tile.title">
+              </v-avatar>
+            </v-list-tile-avatar>
+            <v-list-tile-title>{{ tile.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-bottom-sheet>
+    </div>
     <span>or go back to <router-link to="/login">login</router-link>.</span>
   </div>
 </template>
